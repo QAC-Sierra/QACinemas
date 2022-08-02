5b6ea1e5bee6301aa52be8b6ac345636eeeb1545
Cam-Black <cameronablack1998@gmail.com>
2022-08-02 16:44:35 +0100

    QAC-48 QAC-49 #comment Added required imports for Bootstrap and changed HomePage component to Classifications component to test page working and displaying correctly while awaiting Navbar.

--- qac-site/src/App.js
+++ qac-site/src/App.js
@@ -1,10 +1,13 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import HomePage from "./components/HomePage";
+import Classifications from "./components/Classifications";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Classifications />
     </div>
   );
 }

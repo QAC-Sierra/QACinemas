ba662ef21052a1c5f1eaae7d5b04ad25ffb4c149
Cam-Black <cameronablack1998@gmail.com>
2022-08-02 14:53:36 +0100

    QAC-62 #comment Added Payment function and exported so can be used in other parts of the project.

---
+++ qac-site/src/components/Payment.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Payment = () => {
+	return (
+		<>
+			<header>
+				<h1>You are almost there, please provide your payment details:</h1>
+			</header>
+		</>
+	);
+}
+ export default Payment;

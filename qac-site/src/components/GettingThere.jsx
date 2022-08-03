46a448bbe23128cf3041459fdde8b5da89ebce2b
Cam-Black <cameronablack1998@gmail.com>
2022-08-03 13:27:17 +0100

    QAC-66 #comment Added description to component.

--- qac-site/src/components/GettingThere.jsx
+++ qac-site/src/components/GettingThere.jsx
@@ -5,7 +5,9 @@ const GettingThere = () => {
 		<>
 			<header>
 				<h1>Getting There</h1>
+				<p>Here you can find directions to the venue, information on local public transport and parking.</p>
 			</header>
+			
 		</>
 	);
 }

de3f3e214acfa0a10a3449487926c8d21b67c713
LewisPearsonGitHub <lewiscpearson@gmail.com>
2022-08-03 09:36:14 +0100

    QAC-40 #in-progress #comment Created listings gallery component

---
+++ qac-site/src/components/ListingsGallery.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ListingsGallery = () => {
+	return (
+		<>
+			<header>
+				<h1>
+				This is the listings gallery for the cinema. Here you can see which films are currently being shown.	
+                </h1>
+			</header>
+		</>
+	)
+}
+
+export default ListingsGallery;

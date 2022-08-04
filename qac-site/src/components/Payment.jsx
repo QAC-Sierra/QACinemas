032b436e37ac12e4e74cdb4c46c08f5421414a70
Cam-Black <cameronablack1998@gmail.com>
2022-08-04 16:55:00 +0100

    QAC-63 #comment Removed Row and Col React Bootstrap Elements. Added submit button and blank form label and input.

--- qac-site/src/components/Payment.jsx
+++ qac-site/src/components/Payment.jsx
@@ -1,35 +1,37 @@
 import React from 'react';
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import "./styles/Payment.css"
+import Button from "react-bootstrap/Button";
 
 const Payment = () => {
 	return (
 		<>
 			<Container>
-				<Row>
-					<header>
-						<h1>You are almost there, please provide your payment details:</h1>
-					</header>
-				</Row>
-				<Row>
-					<Form>
-						<Col>
-							<Form.Group>
-								<Form.Label>First Name:</Form.Label>
-								<Form.Control type="text"/>
-							</Form.Group>
-						</Col>
-						<Col>
-							<Form.Group>
-								<Form.Label>Surname:</Form.Label>
-								<Form.Control type="text"/>s
-							</Form.Group>
-						</Col>
-					</Form>
-				</Row>
+				<header>
+					<h1>You are almost there, please provide your payment details:</h1>
+				</header>
+				<Form>
+					<Form.Group>
+						<Form.Label>First Name:</Form.Label>
+						<Form.Control type="text"/>
+					</Form.Group>
+					<Form.Group>
+						<Form.Label>Surname:</Form.Label>
+						<Form.Control type="text"/>
+					</Form.Group>
+					<Form.Group>
+						<Form.Label>Email:</Form.Label>
+						<Form.Control type="email" placeholder="myemail@example.com"/>
+					</Form.Group>
+					<Form.Group>
+						<Form.Label></Form.Label>
+						<Form.Control/>
+					</Form.Group>
+					<Button variant="primary" type="submit">
+						Submit
+					</Button>
+				</Form>
 			</Container>
 		</>
 	);

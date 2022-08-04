42115415e3d4ce9d72c0b3fd2c29130afb77c9b6
Cam-Black <cameronablack1998@gmail.com>
2022-08-04 16:28:48 +0100

    QAC-63 #in-progress #comment Created beginning of Form with First Name Label and input field for text, using React Bootstrap.

--- qac-site/src/components/Payment.jsx
+++ qac-site/src/components/Payment.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
 
 const Payment = () => {
 	return (
 		<>
-			<header>
-				<h1>You are almost there, please provide your payment details:</h1>
-			</header>
+			<Container>
+				<Row>
+					<header>
+						<h1>You are almost there, please provide your payment details:</h1>
+					</header>
+				</Row>
+				<Row>
+					<Form>
+						<Form.Group>
+							<Col>
+								<Form.Label>First Name</Form.Label>
+								<Form.Control type="text"/>
+							</Col>
+						</Form.Group>
+					</Form>
+				</Row>
+			</Container>
 		</>
 	);
 }
- export default Payment;
+export default Payment;

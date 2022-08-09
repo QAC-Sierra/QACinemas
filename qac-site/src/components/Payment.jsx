94b61c9d7cb1e80c18319cfbec552a48a70599f5
Cam-Black <cameronablack1998@gmail.com>
2022-08-09 12:55:08 +0100

    QAC-62 #comment Added Axios to handle http requests

--- qac-site/src/components/Payment.jsx
+++ qac-site/src/components/Payment.jsx
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import StripeCheckout from "react-stripe-checkout";
 import "./styles/Payment.css"
+import axios from "axios";
 
 const KEY = "pk_test_51LTMe7FcNpq5nVHDSFyODWk8Ydr4wnWd65Zea5todzNPVBEqh7UDk9Ka3NDly11dnBRANy6uBhNIDeMudVurCCjB00OyiSEcpE";
 
@@ -12,6 +13,23 @@ const Payment = () => {
 		console.log(stripeToken);
 	}
 	
+	useEffect(() => {
+		const makeRequest = async () => {
+			try {
+				console.log(stripeToken);
+				const res = await axios.post("http://localhost:5000/token/create",
+					{
+						tokenId: stripeToken.id,
+						amount: totalCost,
+						currency: "GBP"
+					}
+				);
+			} catch (err) {
+				console.error(err);
+			}
+		}
+		stripeToken && makeRequest();
+	}, [stripeToken]);
 	
 	return (
 		<>

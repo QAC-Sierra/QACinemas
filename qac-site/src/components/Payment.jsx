bde838431bad2ea4cf1fbfcba01f860298677e4c
Cam-Black <cameronablack1998@gmail.com>
2022-08-09 12:53:36 +0100

    QAC-63 #comment Added StripeCheckout and used attributes, logging them to the console.

--- qac-site/src/components/Payment.jsx
+++ qac-site/src/components/Payment.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
+import StripeCheckout from "react-stripe-checkout";
 import "./styles/Payment.css"
 
+const KEY = "pk_test_51LTMe7FcNpq5nVHDSFyODWk8Ydr4wnWd65Zea5todzNPVBEqh7UDk9Ka3NDly11dnBRANy6uBhNIDeMudVurCCjB00OyiSEcpE";
+
 const Payment = () => {
+	const [stripeToken, setStripeToken] = useState(null);
+	
+	const onToken = async (token) => {
+		await setStripeToken(token);
+		console.log(stripeToken);
+	}
+	
+	
 	return (
 		<>
-
+			<StripeCheckout className="payByCard"
+			                name="QA Cinemas"
+			                description="Payment Details"
+			                billingAddress
+			                amount={200}
+			                currency="GBP"
+			                token={onToken}
+			                stripeKey={KEY}
+			/>
 		</>
 	);
 }

eb7e5b992bb8bf0f3d6c0e353d3fae1142c0245a
Ridwan-Kawsar <ridwan_kawsar@hotmail.co.uk>
2022-08-10 15:43:50 +0100

    QAC-85 #done #comment created a contact us form with validation

--- qac-site/src/components/Contact.jsx
+++ qac-site/src/components/Contact.jsx
@@ -1,11 +1,5 @@
 import React from "react";
-
-// import FloatingLabel from 'react-bootstrap/FloatingLabel';
-// import Button from 'react-bootstrap/Button';
-// import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
-// import Row from 'react-bootstrap/Row';
-
 
 const Contact = () => {
   return (

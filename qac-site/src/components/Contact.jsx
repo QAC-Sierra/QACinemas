3b9eb144823e3e3c943c4c7bd40495df10fb0ecf
Ridwan-Kawsar <ridwan_kawsar@hotmail.co.uk>
2022-08-10 12:18:30 +0100

    QAC-85 #in-progress #comment Created an email contact form with user info

--- qac-site/src/components/Contact.jsx
+++ qac-site/src/components/Contact.jsx
@@ -1,15 +1,42 @@
 import React from "react";
 
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+
+
 const Contact = () => {
-	return (
-		<>
-			<header>
-				<h1>
-				This is the contact page for the QA Cinema Website. 
-                </h1>
-			</header>
-		</>
-	);
+  return (
+<Form>
+      <Row className="mb-3">
+      <Form.Group as={Col} controlId="formGridPassword">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="name" placeholder="Name" />
+        </Form.Group>
+        </Row>
+
+        <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+      </Row>
+
+      <FloatingLabel
+        controlId="floatingTextarea"
+        label="Comments"
+        className="mb-3"
+      >
+        <Form.Control as="textarea" placeholder="Leave a comment here" style={{ height: '100px' }}/>
+      </FloatingLabel>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
 }
 
 export default Contact;

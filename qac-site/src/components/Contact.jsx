8ae5099eb8b7f7c51e2e02c1eccb17d189e56821
Ridwan-Kawsar <ridwan_kawsar@hotmail.co.uk>
2022-08-10 15:40:33 +0100

    QAC-85 #done #comment created a contact us form with validation

--- qac-site/src/components/Contact.jsx
+++ qac-site/src/components/Contact.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
+// import FloatingLabel from 'react-bootstrap/FloatingLabel';
+// import Button from 'react-bootstrap/Button';
+// import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
+// import Row from 'react-bootstrap/Row';
 
 
 const Contact = () => {
@@ -15,36 +15,6 @@ const Contact = () => {
      <input type="comment" name="comment" required></input>
      
      <button type="submit">Send</button>
-
-      {/* <Row className="mb-3">
-      <Form.Group as={Col} controlId="formGridPassword">
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="name" placeholder="Name" />
-        </Form.Group>
-        </Row>
-
-        <Row className="mb-3">
-        <Form.Group as={Col} controlId="formGridEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
-      </Row>
-
-      <FloatingLabel
-        controlId="floatingTextarea"
-        label="Comments"
-        className="mb-3"
-      >
-        <Form.Control as="textarea" placeholder="Leave a comment here" style={{ height: '100px' }}/>
-      </FloatingLabel>
-
-      <Form.Group className="mb-3" id="formGridCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-
-      <Button variant="primary" type="submit">
-        Submit
-      </Button> */}
     </Form>
   );
 }

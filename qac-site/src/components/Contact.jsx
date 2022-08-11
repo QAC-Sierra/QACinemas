94962c5f21e98a42c596797fc34370238290c832
Saiful Hussain <saif.h98@hotmail.com>
2022-08-11 16:44:40 +0100

    QAC-84 #done #comment Contacts page updated with form and mailing address

--- qac-site/src/components/Contact.jsx
+++ qac-site/src/components/Contact.jsx
@@ -1,16 +1,97 @@
 import React from "react";
-import Form from 'react-bootstrap/Form';
+import {
+  CContainer,
+  CFormInput,
+  CForm,
+  CRow,
+  CFormLabel,
+  CCol,
+  CFormCheck,
+  CButton,
+  CFormTextarea,
+  CCard,
+  CCardBody,
+} from "@coreui/react";
+import "./styles/Contact.css"
 
 const Contact = () => {
   return (
-<Form action="https://formsubmit.co/ridwan_kawsar@hotmail.co.uk" method="POST">
-     <input type="text" name="name" required></input>
-     <input type="email" name="email" required></input>
-     <input type="comment" name="comment" required></input>
-     
-     <button type="submit">Send</button>
-    </Form>
+    <CContainer>
+      <br/>
+      <h1 id="Title1"> Contact Us</h1>
+      <h4>
+            {" "}
+            Have a query or an issue? Leave us an Email and we will get back to
+            you within 48 hours!
+          </h4>
+          <br></br>
+      <CCard className="Card" style={{ width: "40rem" }}>
+        <CCardBody>
+          <CForm
+            action="https://formsubmit.co/saifhussain98@outlook.com"
+            method="POST"
+          >
+            <CRow className="mb-3">
+              <CCol sm={10}>
+                <CFormInput
+                  required
+                  type="text"
+                  placeholder="Name"
+                  name="name"
+                  id="NameBox"
+                  style={{ margin: "auto" }}
+                />
+              </CCol>
+            </CRow>
+
+            <CRow className="mb-3">
+              <CCol sm={10}>
+                <CFormInput
+                  required
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  id="EmailBox"
+                />
+              </CCol>
+            </CRow>
+
+            <CRow className="mb-4">
+            <CCol sm={10}>
+              <CFormTextarea
+                required
+                type="comment"
+                placeholder="Comment"
+                name="comment"
+                id="CommentBox"
+                rows="3"
+              />
+              </CCol>
+            </CRow>
+
+            <div className="d-grid d-md-flex justify-content-md-left">
+              <CButton type="submit">Send</CButton>
+            </div>
+          </CForm>
+        </CCardBody>
+      </CCard>
+
+      <p id="ContactText">
+        Alternatively, if you would like to write to us, please mail to:
+        <br/>
+        Training Facility,
+        <br/>
+        Address Line 1,
+        <br/>
+        Address Line 2,
+        <br/>
+        City,
+        <br/>
+        PostCode
+      </p>
+
+    </CContainer>
   );
-}
+};
 
 export default Contact;
